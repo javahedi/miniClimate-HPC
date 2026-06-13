237813de06cfe9b9cdb9300020df65532457ef1a
javahedi <javahedi@gmail.com>
2026-06-13 20:25:43 +0200

    Initial serial diffusion solver with config parsing

---
+++ build/CMakeFiles/miniClimate.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/javadvahedi/Desktop/project-miniClimte-HPC/src/config.cpp" "CMakeFiles/miniClimate.dir/src/config.cpp.o" "gcc" "CMakeFiles/miniClimate.dir/src/config.cpp.o.d"
+  "/Users/javadvahedi/Desktop/project-miniClimte-HPC/src/main.cpp" "CMakeFiles/miniClimate.dir/src/main.cpp.o" "gcc" "CMakeFiles/miniClimate.dir/src/main.cpp.o.d"
+  "/Users/javadvahedi/Desktop/project-miniClimte-HPC/src/solver.cpp" "CMakeFiles/miniClimate.dir/src/solver.cpp.o" "gcc" "CMakeFiles/miniClimate.dir/src/solver.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/miniClimate.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for miniClimate.
+# This may be replaced when dependencies are built.
